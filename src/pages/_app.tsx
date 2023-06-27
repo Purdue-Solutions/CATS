03acd997d6d05d4b1be8edaa94c7d02288006b2f
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-06-26 23:55:10 -0700

    Firebase App & Auth Setup Done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
-
-import { useState } from 'react';
+import StyleProvider from '@/providers/StyleProvider';
+import AuthProvider from '@/providers/AuthProvider';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-
-
+  
   return (
     <>
       <Head>
@@ -19,15 +14,11 @@ export default function App(props: AppProps) {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
 
-      <ColorSchemeProvider colorScheme={ colorScheme } toggleColorScheme={ toggleColorScheme }>
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={{ colorScheme, primaryColor: 'yellow' }}
-        >
+      <StyleProvider>
+        <AuthProvider>
           <Component {...pageProps} />
-        </MantineProvider>
-      </ColorSchemeProvider>
+        </AuthProvider>
+      </StyleProvider>
     </>
   );
 }
