ebcdb46ac99633bf0cd657416cd220d984411ef2
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-07-12 21:19:16 -0700

    email link sign in complete

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -7,18 +7,24 @@ import {
   Container,
   Button,
   Center,
+  Group,
+  Flex,
 } from '@mantine/core';
 import { useInputState } from '@mantine/hooks';
 import { notifications } from '@mantine/notifications';
-import { IconCheck, IconX } from '@tabler/icons-react';
+import { IconMail, IconX } from '@tabler/icons-react';
 import MainLogo from '@/components/misc/MainLogo';
 import { sendSignInEmail } from '@/lib/firebase/utils';
 import { AuthError } from 'firebase/auth';
+import { useState } from 'react';
 
 const purdueEmailRegex = /[a-zA-Z0-9_.-]+@purdue.edu$/
 
 export default function Login() {
+  const [firstName, setFirstName] = useInputState('');
+  const [lastName, setLastName] = useInputState('');
   const [email, setEmail] = useInputState('');
+  const [loginDone, setLoginDone] = useState(false);
   const emailError = email && !purdueEmailRegex.test(email) ? 
     <Text color='red' size="sm">Invalid Purdue email</Text> : null
   
@@ -26,13 +32,7 @@ export default function Login() {
   const createAccount = async () => {
     try {
       await sendSignInEmail(email);
-      notifications.show({
-        title: 'Sign In Email',
-        color: 'green',
-        message: `An email was sent to ${email}. You can sign in through the link in the email. Please check your spam!`,
-        icon: <IconCheck />,
-        autoClose: 1000000
-      })
+      setLoginDone(true);
     }
     catch (error) {
       notifications.show({
@@ -45,41 +45,81 @@ export default function Login() {
     }
   }
 
+  const Login = (
+  <Container size={480} my={40}>
+    <Title
+      align="center"
+      id="welcome-back"
+      sx={(theme) => ({ fontFamily: `${theme.fontFamily}`, fontWeight: 900 })}
+    >
+      {"Let's get started"}
+    </Title>
+    <Text color="dimmed" size="sm" align="center" mt={5}>
+      Not sure what you are getting into?{' '}
+      <Anchor size="sm" href='https://www.purduesolutions.org' id='log-in'>
+        Learn More
+      </Anchor>
+    </Text>
+
+    <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+      {/* <Flex justify="space-between">
+        <TextInput label="First Name" placeholder="Purdue" 
+          onChange={ setFirstName } required id="email-input"/>
+        <TextInput label="Last Name" placeholder="Pete" 
+          onChange={ setLastName } required id="email-input"/>
+      </Flex> */}
+      <TextInput label="Purdue Email" placeholder="pete@purdue.edu" 
+        onChange={ setEmail } required id="email-input" mt={10}/>
+      { emailError }
+      <Button 
+        fullWidth
+        mt="xl" 
+        id="sign-up"
+        disabled={ (email == '') || (emailError != null) }
+        onClick={ createAccount }
+      >
+        Sign In
+      </Button>
+    </Paper>
+  </Container>);
+
+const LoginDone = (
+  <Container size={420} my={40}>
+    <Title
+      align="center"
+      id="sent-email"
+      sx={(theme) => ({ fontFamily: `${theme.fontFamily}`, fontWeight: 900 })}
+    >
+      {"We sent you a login link"}
+    </Title>
+    <Text color="dimmed" size="sm" align="center" mt={5}>
+      {`If you don't see it, please wait up to 5 minutes and check your spam!`}
+    </Text>
+
+    <Center>
+      <Button
+        component="a"
+        href='https://outlook.office.com/mail/'
+        target='_blank'
+        mt="xl"
+        id="open-outlook"
+        color="blue"
+        variant="subtle"
+        leftIcon={<IconMail />}
+      >
+        Open Outlook
+      </Button>
+    </Center>
+  </Container>);
+
+  
+
   return (
     <>
       <Center mt={40}>
         <MainLogo width={200} />
       </Center>
-      <Container size={420} my={40}>
-        <Title
-          align="center"
-          id="welcome-back"
-          sx={(theme) => ({ fontFamily: `${theme.fontFamily}`, fontWeight: 900 })}
-        >
-          {"Let's get started"}
-        </Title>
-        <Text color="dimmed" size="sm" align="center" mt={5}>
-          Not sure what you are getting into?{' '}
-          <Anchor size="sm" href='https://www.purduesolutions.org' id='log-in'>
-            Learn More
-          </Anchor>
-        </Text>
-
-        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-          <TextInput label="Purdue Email" placeholder="pete@purdue.edu" 
-            onChange={setEmail} required id="email-input"/>
-          { emailError }
-          <Button 
-            fullWidth
-            mt="xl" 
-            id="sign-up"
-            disabled={ (email == '') || (emailError != null) }
-            onClick={ createAccount }
-          >
-            Sign In
-          </Button>
-        </Paper>
-      </Container>
+      { loginDone ? LoginDone : Login }
     </>
   );
 }
